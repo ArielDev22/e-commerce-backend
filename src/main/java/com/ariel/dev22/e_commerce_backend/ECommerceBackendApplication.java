81377685ace697bd82da0f15640f59d812377977
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-10-14 17:03:18 -0300

    Initial commit

---
+++ src/main/java/com/ariel/dev22/e_commerce_backend/ECommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ariel.dev22.e_commerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ariel/dev22/e_commerce_backend/ECommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ariel.dev22.e_commerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
