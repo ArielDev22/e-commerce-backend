c34b257b42caf874f82dfe6e02dd77f31f6a4c1f
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-11-12 10:22:57 -0300

    Melhorias no filter de segurança

--- src/main/java/com/ariel/dev22/e_commerce_backend/security/SecurityFilter.java
+++ src/main/java/com/ariel/dev22/e_commerce_backend/security/SecurityFilter.java
@@ -1,8 +1,8 @@
 package com.ariel.dev22.e_commerce_backend.security;
 
 import com.ariel.dev22.e_commerce_backend.auth.service.AuthService;
-import com.ariel.dev22.e_commerce_backend.token.service.TokenService;
 import com.ariel.dev22.e_commerce_backend.token.service.RevokedTokenService;
+import com.ariel.dev22.e_commerce_backend.token.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -27,17 +27,19 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-
         String token = extractToken(request);
 
-        if (token != null && !revokedTokenService.isRevoked(token)) {
+        if (token != null) {
             String email = tokenService.validateToken(token);
+
             if (email != null) {
                 UserDetails user = authService.loadUserByUsername(email);
 
                 var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                System.out.println(revokedTokenService.revokeToken(token));
             }
         }
         filterChain.doFilter(request, response);
