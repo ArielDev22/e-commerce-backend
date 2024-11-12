9c5fd7ef004783752c7941750ba3ee9e54b606f1
Ariel Rodrigues <ariel.dev22@gmail.com>
2024-11-12 10:27:17 -0300

    Alterações no Controller de favoritos

--- src/main/java/com/ariel/dev22/e_commerce_backend/favorite/controller/FavoriteController.java
+++ src/main/java/com/ariel/dev22/e_commerce_backend/favorite/controller/FavoriteController.java
@@ -1,9 +1,9 @@
 package com.ariel.dev22.e_commerce_backend.favorite.controller;
 
-import com.ariel.dev22.e_commerce_backend.favorite.service.FavoriteService;
-import com.ariel.dev22.e_commerce_backend.favorite.models.FavoriteItem;
 import com.ariel.dev22.e_commerce_backend.favorite.dto.FavoriteItemDTO;
-import com.ariel.dev22.e_commerce_backend.product.dto.ProductIdDTO;
+import com.ariel.dev22.e_commerce_backend.favorite.models.FavoriteItem;
+import com.ariel.dev22.e_commerce_backend.favorite.service.FavoriteService;
+import com.ariel.dev22.e_commerce_backend.product.dto.ProductId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,25 +20,24 @@ public class FavoriteController {
     private final FavoriteService favoriteService;
 
     @PostMapping(value = "/add")
-    public ResponseEntity<String> addItem(@RequestBody ProductIdDTO id,
-                                          @AuthenticationPrincipal UserDetails userDetails) {
-        return ResponseEntity.ok(favoriteService.addItem(id.productId(), userDetails.getUsername()));
+    public ResponseEntity<String> addProductToFavorites(@RequestBody ProductId id,
+                                                        @AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(favoriteService.addProductToFavorites(id.productId(), userDetails.getUsername()));
     }
 
     @GetMapping
-    public ResponseEntity<List<FavoriteItemDTO>> listItems(@AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<List<FavoriteItemDTO>> listAllItems(@AuthenticationPrincipal UserDetails userDetails) {
         List<FavoriteItemDTO> items = new ArrayList<>();
 
-        for (FavoriteItem i : favoriteService.listItems(userDetails.getUsername())) {
-            items.add(new FavoriteItemDTO(i.getProduct().getId(), i.getName(), i.getPrice(), i.getImageUrl(), i.getAddedAt()));
+        for (FavoriteItem i : favoriteService.listAllItems(userDetails.getUsername())) {
+            items.add(new FavoriteItemDTO(i.getProduct().getId(), i.getName(), i.getUnitPrice(), i.getImageUrl(), i.getAddedAt()));
         }
-
         return ResponseEntity.ok(items);
     }
 
     @DeleteMapping(value = "/remove/{itemId}")
-    ResponseEntity<String> removeItem(@PathVariable Long itemId,
-                                      @AuthenticationPrincipal UserDetails userDetails) {
-        return ResponseEntity.ok(favoriteService.removeItem(itemId, userDetails.getUsername()));
+    ResponseEntity<String> removeItemFromFavorites(@PathVariable Long itemId,
+                                                   @AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(favoriteService.removeItemFromFavorites(itemId, userDetails.getUsername()));
     }
 }
